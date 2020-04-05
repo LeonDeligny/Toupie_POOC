2d5f57910fb70ec4a80239fa290ca301b62b64e8
Bruce Wayne <61823638+AlexLeonproj@users.noreply.github.com>
2020-04-05 17:03:06 +0200

    Update TextViewer.cpp

--- TextViewer.cpp
+++ TextViewer.cpp
@@ -1,28 +1,21 @@
-//
-//  TextViewer.cpp
-//  POO
-//
-//  Created by Léon Deligny on 26/03/2020.
-//  Copyright © 2020 Léon Deligny. All rights reserved.
-//
-
 #include <iostream>
 #include "TextViewer.hpp"
 #include "Contenu.hpp"
 
-void TextViewer::dessine(Contenu const&){
-    flot << "un cube" << std::endl;
-}
-
 void TextViewer::dessine(Toupie const &a_dessiner) {
-    flot << "Une toupie" << std::endl;
+    flot << "paramètres : " << a_dessiner.get.parametres();
 }
 
 void TextViewer::dessine(Systeme const &a_dessiner) {
-    flot<< " Un systeme" << std::endl;
+    ensemble.dessiner(flot);
+    /* for (size_t i(0); i<ensemble.get_size(); ++i){
+    flot << "dessin de la toupie " << i << " : ";
+    ensemble.get_toupie(i)->dessine();
+    }*/
 }
 
 void TextViewer::dessine(ConeSimple const &a_dessiner) {
-    flot << "Un cone simple" << std::endl;
+    flot << "parametres : " << a_dessiner.get_parametres();
+         <<"; derivees : " << a_dessiner.get_derivees()<<std::endl;
 }
 
