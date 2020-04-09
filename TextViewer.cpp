0d20c6b61455200189a0ee10c583fa5e2c4261ec
Bruce Wayne <61823638+AlexLeonproj@users.noreply.github.com>
2020-04-09 08:46:59 +0200

    Update TextViewer.cpp

--- TextViewer.cpp
+++ TextViewer.cpp
@@ -2,20 +2,21 @@
 #include "TextViewer.hpp"
 #include "Contenu.hpp"
 
-void TextViewer::dessine(Toupie const &a_dessiner) {
-    flot << "paramètres : " << a_dessiner.get_derivees_();
+void TextViewer::dessine(Toupie const &toupie) {
+    flot << "paramètres : " << toupie.getparametres();
 }
 
-void TextViewer::dessine(Systeme const &a_dessiner) {
-    ensemble.dessiner(flot);
+void TextViewer::dessine(Systeme const &toupies) {
+    toupies.dessiner(flot);
     /* for (size_t i(0); i<ensemble.get_size(); ++i){
     flot << "dessin de la toupie " << i << " : ";
     ensemble.get_toupie(i)->dessine();
     }*/
 }
 
-void TextViewer::dessine(ConeSimple const &a_dessiner) {
-    flot << "parametres : " << a_dessiner.get_angle();
-         <<"; derivees : " << a_dessiner.get_vitesse()<<std::endl;
+void TextViewer::dessine(ConeSimple const &conesimple) {
+    flot << "parametres : " << conesimple.get_parametres();
+    flot <<"; derivees : " << conesimple.get_derivees_()<<std::endl;
 }
 
+//Rien ne compile
