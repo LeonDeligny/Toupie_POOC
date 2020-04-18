75afa46f8696430317f1bdfd6f99fba98afb1d69
Bruce Wayne <61823638+AlexLeonproj@users.noreply.github.com>
2020-04-18 17:07:12 +0200

    Update IntegrateurEulerCromer.cpp

--- IntegrateurEulerCromer.cpp
+++ IntegrateurEulerCromer.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 void Integrateur_Euler_Cromer::evolue(Toupie &spin, const double& dt) {
+    /*Les formules viennent du complément mathématique, le corps de la fonction f() est dans la classe
+    conesimple.
+    les deux cout plus bas sont là pour simplifier une méthode de la classe systeme, et donc rajoutés 
+    bien plus tardivement.*/
     spin.set_derivees(spin.get_derivees_() + dt * spin.f());
     
     spin.set_parametres(spin.get_parametres() + dt * spin.get_derivees_());
