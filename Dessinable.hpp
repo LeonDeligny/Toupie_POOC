78ef3328e7957474f2c00a75fabf4106fb6f174e
Bruce Wayne <61823638+AlexLeonproj@users.noreply.github.com>
2020-04-23 11:39:23 +0200

    Update Dessinable.hpp

--- Dessinable.hpp
+++ Dessinable.hpp
@@ -2,14 +2,16 @@
 #ifndef Dessinable_hpp
 #define Dessinable_hpp
 
-class SupportADessin;
+class SupportADessin; 
+//Cette portion de code est nécessaire pour les attributs de dessinable, et pour éviter
+//les include qui ne fonctionnaient pas
 
 class Dessinable {
 protected:
-    SupportADessin* support;
+    SupportADessin* support; //Un pointeur sur un support à dessin, comme demandé
 public:
-    virtual void dessine() = 0;
-    virtual ~Dessinable() = default;
+    virtual void dessine() = 0; //La méthode est redéfinie dans les classes qui héritent de dessinable : toupie, conesimple, etc
+    virtual ~Dessinable() = default; //Assure le polymorphisme dans les sous-classes
     Dessinable(SupportADessin* support)
     : support(support) { }
 };
